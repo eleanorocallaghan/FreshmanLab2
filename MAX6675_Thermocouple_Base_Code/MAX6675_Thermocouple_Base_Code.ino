222bf1e6514cb1a1a31d873ffa3586314c9815c9
eleanorocallaghan <eleanor.ocallaghan@gmail.com>
2019-02-16 21:22:22 -0500

    formatted and added comment

--- MAX6675_Thermocouple_Base_Code/MAX6675_Thermocouple_Base_Code.ino
+++ MAX6675_Thermocouple_Base_Code/MAX6675_Thermocouple_Base_Code.ino
@@ -2,6 +2,8 @@
 //Include library for TC board
 #include "max6675.h"
 
+//github test
+
 int SO = 8; //Looks like "90" on the TC board
 int CS = 9;
 int CLK = 10; //"SCK" on the TC board
@@ -20,7 +22,7 @@ int redLEDPWMValue; //This will be a number between 0 (0v) and 255 (5v)
 
 MAX6675 thermoCouple(CLK, CS, SO);
 
-  
+
 void setup() {
   Serial.begin(9600);
   // give the MAX a little time to settle
@@ -30,28 +32,28 @@ void setup() {
 void loop() {
 
   //Read the temperature in Celsius
-  degreeCelsius=thermoCouple.readCelsius();
+  degreeCelsius = thermoCouple.readCelsius();
 
-  blueLEDVoltage = 5*degreeCelsius/100.0;
+  blueLEDVoltage = 5 * degreeCelsius / 100.0;
 
-  if (blueLEDVoltage<=5)
+  if (blueLEDVoltage <= 5)
   {
-   blueLEDPWMValue=255-255*(blueLEDVoltage)/5.; 
+    blueLEDPWMValue = 255 - 255 * (blueLEDVoltage) / 5.;
   }
   else
   {
-    blueLEDPWMValue=0;
+    blueLEDPWMValue = 0;
   }
 
- redLEDVoltage = 5*degreeCelsius/100.0;
+  redLEDVoltage = 5 * degreeCelsius / 100.0;
 
-  if (redLEDVoltage<=5)
+  if (redLEDVoltage <= 5)
   {
-   redLEDPWMValue=255*(blueLEDVoltage)/5.; 
+    redLEDPWMValue = 255 * (blueLEDVoltage) / 5.;
   }
   else
   {
-    redLEDPWMValue=0;
+    redLEDPWMValue = 0;
   }
 
   analogWrite(redLEDPin, redLEDPWMValue);
@@ -63,6 +65,6 @@ void loop() {
   Serial.print('\t');
   Serial.print(degreeCelsius);
   Serial.println();
- 
+
   delay(200); //Don't decrease this, or board may not work
 }
